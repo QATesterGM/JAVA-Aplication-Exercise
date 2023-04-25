f80332b63df22b9c17487193b6d8f14b35305ec0
Lukasz <gajdalukasz89@gmail.com>
2023-04-25 09:57:26 +0200

    static final explanation

---
+++ src/utils/WeekUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class WeekUtils {
+
+    // final znaczy, że to pole zostało raz zdefiniowanane i nigdy nie zostanie zmienione.
+    // druga rzecz, zawsze nazwę takiego pola final piszemy dużymi literami
+
+    public static final String MONDAY = "Monday";
+
+}
